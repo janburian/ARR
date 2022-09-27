e3416f8d3af4d3a7e37b229f84d119fa156619c1
janburian <jburian@students.zcu.cz>
2022-09-27 14:54:03 +0200

    First commit

---
+++ OneFilter.m
@@ -0,0 +1,17 @@
+function [hodnota] = OneFilter(frekvenceHz, cisloFiltru, bm)
+hodnota = 0; 
+
+if ((bm(cisloFiltru) < frekvenceHz) && (frekvenceHz <= bm(cisloFiltru + 1)))
+    hodnota = (frekvenceHz - bm(cisloFiltru)) / (bm(cisloFiltru+1) - bm(cisloFiltru));
+end
+
+if ((bm(cisloFiltru + 1) < frekvenceHz) && (frekvenceHz <= bm(cisloFiltru + 2)))
+    hodnota = (frekvenceHz - bm(cisloFiltru + 2)) / (bm(cisloFiltru + 1) - bm(cisloFiltru + 2));
+end
+
+%FILTERS Summary of this function goes here
+%   hranice vektor hranic filtru 
+%   cisloFiltru - ktery filtr vybirame
+%   Hz 
+
+
---
+++ hz2mel.m
@@ -0,0 +1,4 @@
+function z = hz2mel(f)
+
+z = 2595 * log10(1 + f/700);
+
