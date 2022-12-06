d66980dd1a7fd296184b85fdd5218e196bc99060
janburian <jburian@students.zcu.cz>
2022-12-06 16:41:18 +0100

    Added method for loading leaves

--- Jazykovy model/dekoder.py
+++ Jazykovy model/dekoder.py
@@ -13,14 +13,35 @@ def load_vocab_file(filename: str):
             list_transcript = list(transcript)
             list_final.append(list_transcript)
     file.close()
+
     return list_final
 
-# TODO nacist leaves.txt
+def load_leaves(filename: str):
+    '''
+    Loads from leaves file (e. g. a prob prob; b prob prob)
+    :param filename:
+    :return:
+    '''
+    dictionary = {}
+    with open(filename, 'r', encoding='cp1250') as file:
+        for line in file:
+            line_final = (line.rstrip("\n")).split()
+
+            char = line_final[0]
+            probabilities = line_final[1:]
+
+            dictionary[char] = probabilities
+    file.close()
+
+    return dictionary
+
 
 if __name__ == "__main__":
     vocab_filename = "vocab"
+    leaves_filename = "leaves.txt"
 
     mesh = load_vocab_file(vocab_filename)
+    leaves_dict = load_leaves(leaves_filename)
     print()
 
 
