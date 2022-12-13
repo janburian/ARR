be8ba60a83f2961a5e5dfb3a524f383757679c62
janburian <jburian@students.zcu.cz>
2022-12-13 13:45:43 +0100

    Beginning of Viterbi

--- Jazykovy model/dekoder.py
+++ Jazykovy model/dekoder.py
@@ -1,3 +1,6 @@
+import math
+
+
 def load_vocab_file(filename: str):
     '''
     Loads vocab file
@@ -35,13 +38,82 @@ def load_leaves(filename: str):
 
     return dictionary
 
+def load_probabilities(filename: str):
+    list_final = []
+    with open(filename, 'r', encoding='cp1250') as file:
+        for line in file:
+            line_final = (line.rstrip("\n")).split()
+
+            line_final_list = list(line_final)
+            list_final.append(line_final_list)
+        file.close()
+
+    return list_final
+
+def viterbi(probs: list, leaves: dict, mesh: dict):
+    prices_init = []
+
+    # Inicializace (radky = casy, sloupce = fonemy)
+    N = len(leaves.keys())
+    T = len(probs)
+
+    values = []
+
+    for word in mesh:
+        values = []
+        for j in range(0, N):
+            first_char = word[0]
+            idx = list(leaves.keys()).index(first_char)
+            if j == 0:
+                list_stavu = probs[j] # prvni radka
+                first_value = float(list_stavu[idx])
+                log = -math.log10(first_value)
+                values.append(log)
+            else:
+                values.append(math.inf)
+
+        prices_init.append(values)
+
+    print()
+    prices_init = []
+
+
+    # Iterativni vypocet
+    '''
+    min = 50
+
+    for list in prices_init:
+        tmp = min(list)
+        if tmp < min:
+            min = tmp
+        continue
+    '''
+
+
+    for t in range(1, T):
+        word = mesh[t]
+        N = len(word)
+
+        for j in range(0, N):
+            first_char = word[j]
+            char_idx = list(leaves.keys()).index(first_char)
+            prices = prices_init[j]
+
+
+
+
+
+
 
 if __name__ == "__main__":
     vocab_filename = "vocab"
     leaves_filename = "leaves.txt"
+    probs_filename = "00170005_14.txt"
 
     mesh = load_vocab_file(vocab_filename)
+    probs = load_probabilities(probs_filename)
     leaves_dict = load_leaves(leaves_filename)
+    viterbi(probs, leaves_dict, mesh)
     print()
 
 
